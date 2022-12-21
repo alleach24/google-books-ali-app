6778ef0f377d3aa11c82248d83c7d5655d5cace1
alleach24 <alleach24@gmail.com>
2022-12-21 01:52:56 -0500

    made user functionality

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 # npm install yargs@13.2
 # npm install axios
 # npm install prompt-sync
+# npm install inquirer
--- bin/index.js
+++ bin/index.js
@@ -1,33 +1,111 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
-const axios = require("axios");
-const prompt = require('prompt-sync')();
+// const yargs = require("yargs");
+// const axios = require("axios");
+import axios from 'axios';
+import promptSync from 'prompt-sync';
+// const prompt = require('prompt-sync')();
+import inquirer from 'inquirer';
+import { read } from 'fs';
+
+
+let readingLists = [];
 
 
 console.log('Welcome to this Google Books API CLI app!');
-
-
-const viewBooks = (data) => {
-    // console.log('hit viewBooks function')
-    // console.log(data.items)
-    for (let i=0; i < 5; i++) {
-        // console.log(data.items[i].volumeInfo)
-        console.log(`Title: ${data.items[i].volumeInfo.title}`)
-        console.log(`Author: ${data.items[i].volumeInfo.authors}`)
-        console.log(`Publisher: ${data.items[i].volumeInfo.publisher}`)
-    }
+async function main() {
+    const userdo = await inquirer.prompt({
+        name: 'do',
+        type: 'list',
+        message:'What would you like to do?',
+        choices: [
+            "Search for books",
+            "View reading lists",
+            "Quit application"
+        ]
+    })
+    if (userdo.do == "Search for books") {
+        console.log('You selected "Search for books"');
+        searchBooks();
+    } else if (userdo.do == "View reading lists") {
+        console.log('You selected "View reading lists"');
+        viewReadingLists();
+    } 
 }
+main()
+
+
 const retrieveBooks = (url) => {
     // console.log('hit retrieveBooks function')
     // console.log(url)
     axios.get(url).then(function (res) {
-        // console.log(res.data)
-        viewBooks(res.data)
+        addBooksToList(res.data)
     });
 }
+async function addBooksToList(data) {
+    const book = await inquirer.prompt({
+        name: 'viewBook',
+        type: 'list',
+        message:'Select a book to add to a reading list',
+        choices: [
+            `"${data.items[0].volumeInfo.title}" by ${data.items[0].volumeInfo.authors}, published by ${data.items[0].volumeInfo.publisher}.`,
+            `"${data.items[1].volumeInfo.title}" by ${data.items[0].volumeInfo.authors}, published by ${data.items[0].volumeInfo.publisher}.`,
+            `"${data.items[2].volumeInfo.title}" by ${data.items[0].volumeInfo.authors}, published by ${data.items[0].volumeInfo.publisher}.`,
+            `"${data.items[3].volumeInfo.title}" by ${data.items[0].volumeInfo.authors}, published by ${data.items[0].volumeInfo.publisher}.`,
+            `"${data.items[4].volumeInfo.title}" by ${data.items[0].volumeInfo.authors}, published by ${data.items[0].volumeInfo.publisher}.`,
+            // do later: handle if the data has less than 5 results
+        ]
+    })
+    console.log(`Saving ${book.viewBook} to Reading List`)
+    const list = await inquirer.prompt({
+        name: 'lists',
+        type: 'list',
+        message:'Which reading list should it be saved to: ',
+        choices: [
+            [...readingLists],
+            "New List"
+        ]
+    })
+    if (list.lists == "New List") {
+        console.log('make a new list')
+        let title = makeList()
+        addBookToList(book.viewBook, title)
+    } else {
+        addBookToList(book.viewBook, list.lists)
+    }
+    console.log("Book successfully added")
+    main()
+    
+}
+function addBookToList(book, list) {
+}
+function makeList() {
+    let prompt = promptSync();
+    let title = prompt('Enter the list title: ')
+    readingLists.push([title, []])
+    return title
+}
+function searchBooks() {
+    let prompt = promptSync();
+    let search = prompt('Enter a search query: ')
+    while (!search){
+        console.log("Please enter a valid search term.");
+        search = prompt('Enter a query: ');
+    }
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${search}` 
+    // console.log(url)
+    retrieveBooks(url)
+}
 
-const search = prompt('Enter a query: ');
-let url = search ? `https://www.googleapis.com/books/v1/volumes?q=${search}` : "https://www.googleapis.com/books/v1/volumes"
-// console.log(url)
-retrieveBooks(url)
+
+
+async function viewReadingLists() {
+    const readingList = await inquirer.prompt({
+        name: 'readingLists',
+        type: 'list',
+        message:'Reading Lists:',
+        choices: readingLists, //handle if readingLists is blank
+    })
+    console.log(readingList)
+    console.log(readingLists)
+}
